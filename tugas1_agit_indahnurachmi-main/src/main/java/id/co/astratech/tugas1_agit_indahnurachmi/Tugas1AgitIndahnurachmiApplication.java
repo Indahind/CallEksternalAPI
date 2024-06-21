9096643fe3ed0f586f8a0262092ccee407c507f2
Indahind <indrayanaindah28@gmail.com>
2024-06-21 14:13:53 +0700

    push

---
+++ tugas1_agit_indahnurachmi-main/src/main/java/id/co/astratech/tugas1_agit_indahnurachmi/Tugas1AgitIndahnurachmiApplication.java
@@ -0,0 +1,13 @@
+package id.co.astratech.tugas1_agit_indahnurachmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas1AgitIndahnurachmiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tugas1AgitIndahnurachmiApplication.class, args);
+    }
+
+}
---
+++ tugas1_agit_indahnurachmi-main/src/test/java/id/co/astratech/tugas1_agit_indahnurachmi/Tugas1AgitIndahnurachmiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.astratech.tugas1_agit_indahnurachmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tugas1AgitIndahnurachmiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
